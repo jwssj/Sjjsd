1409b1e302a12374d66840eb5b1a2b56902ee7c4
jwssj <boyk.91322@gmail.com>
2025-03-19 10:20:56 +0530

    Add files via upload

---
+++ KUNAL.  x.  MODS. V1.lua
@@ -0,0 +1,373 @@
+--[[
+© Dec By メ SHIV MODS  亗 
+
+Eɴᴄʀʏᴘᴛɪᴏɴ: ?
+Vᴇʀsɪᴏɴ: ?
+Gᴀᴍᴇ: ?
+
+Jᴏɪɴ Mʏ CHANNEL Tᴇʟᴇɢʀᴀᴍ
+• Jᴏɪɴ Mʏ Cʜᴀɴɴᴇʟ 
+  ➥ @NGSHIVANGYT
+  ➥ @NGSHIVANGYT
+-------------------------------
+ • Jᴏɪɴ Mʏ YT Cʜᴀɴɴᴇʟ
+  ➥  @X GOD CHEATS OFFICIAL 
+  ➥ @X GOD CHEATS OFFICIAL 
+]]
+--Fᴜᴄᴋ Fᴏʀ Yᴏᴜ Rᴇᴍᴏᴠᴇ Cʀᴇᴅɪᴛ
+
+  gg.alert("🔥メ KUNAL MODS  V13 🌹🔥\n⭐💖 PAID SCRIPT 🎄☃️", "⚡メ KUNAL MODS  V13 🌹Menu⚡")
+  function script()
+    menuprincipal = gg.multiChoice({
+      "Antena Hand🗼",
+      "Night Mode🌚",
+      "MAGIC BULLET🎯",
+      "No Recoil🦠",
+      "Aim Awm🔫",
+      "Awm Switch⏩",
+      "❌Exit🔙"
+    }, nil, "🎅🏻Script Vip Premium🎅🏻\n🌠Creador: KUNAL MODS  Official Team ™🌠\n☃️Youtube: KUNAL MODS ™☃️\n📅 Expired Date:04.02.2025")
+    if menuprincipal == nil then
+      gg.toast("☃️Subscríbete K KUNAL MODS ™☃️")
+    else
+      if menuprincipal[1] then
+        L1()
+      end
+      if menuprincipal[2] then
+        L2()
+      end
+      if menuprincipal[3] then
+        ghost()
+      end
+      if menuprincipal[4] then
+        gpsmano()
+      end
+      if menuprincipal[5] then
+        cielonegro()
+      end
+      if menuprincipal[6] then
+        L7()
+      end
+      if menuprincipal[7] then
+        recoil()
+      end
+      if menuprincipal[8] then
+        aimawm()
+      end
+      if menuprincipal[9] then
+        switch()
+      end
+      if menuprincipal[10] then
+        aimbotmira()
+      end
+      if menuprincipal[11] then
+        lloc()
+      end
+      if menuprincipal[12] then
+        jsped()
+      end
+      if menuprincipal[13] then
+        full()
+      end
+      if menuprincipal[14] then
+        gg.setVisible(true)
+        os.exit(print("☃️ Subscribe メ SHIV MODS 🌹TEAM ™☃️"))
+      end
+    end
+  end
+  
+  function L1()
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 B0 01 CD E1")
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 90 00 9F E5", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 9 ᴏɴ✔️️")
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 02", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 4 ᴏɴ✔️️")
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 03", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 3 ᴏɴ✔️️")
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h F6 0D EA", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 1 ᴏɴ✔️️")
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 B0 01 CD E1")
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 90 00 9F E5", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 9 ᴏɴ✔️️")
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 02", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 4 ᴏɴ✔️️")
+    gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP)
+    gg.searchNumber("h 0A 00 A0 E3 03", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll("h 00 F0 20 E3", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Bypass 3 ᴏɴ✔️️")
+  end
+  
+  function L2()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 61 00 77 00 6D 00,1")
+    gg.getResults(500)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F006006100730065007200", gg.TYPE_BYTE)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 6B 00 61 00 72 00 39 00 38 00 6B 00,1")
+    gg.getResults(500)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F006006100730065007200", gg.TYPE_BYTE)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 6D 00 32 00 34 00,1")
+    gg.getResults(500)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F006006100730065007200", gg.TYPE_BYTE)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 62 00 6D 00 39 00 34 00,1")
+    gg.getResults(500)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F006006100730065007200", gg.TYPE_BYTE)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 61 00 6D 00 6D 00 6F 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 61 00 6D 00 6D 00 6F 00 5F 00 73 00 6E 00 67 00,1")
+    gg.getResults(500)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F0060061007300650072005F0067007200650065006E00", gg.TYPE_BYTE)
+    gg.toast("✅BYPASS ACTIVATED✅")
+  end
+  
+  function ghost()
+    if ghost == tat then
+      SetValue("libil2cpp.so", "0x18f4180", "h 00 00 A0 E3 1E FF 2F E1", 32)
+       gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+      gg.toast(on)
+      ghost = bat
+    elseif ghost == bat then
+      SetValue("libil2cpp.so", "0x18f4180", "h F0 4F 2D E9 1C B0 8D E2", 32)
+       gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+      gg.toast(off)
+      ghost = tat
+    end
+  end
+  
+  function gpsmano()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("-0.02980032004;1;0.48141112924::9", gg.TYPE_FLOAT)
+    gg.refineNumber("1", gg.TYPE_FLOAT)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("3000", gg.TYPE_FLOAT)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.searchNumber("0.09043131769;1;0.14753369987::9", gg.TYPE_FLOAT)
+    gg.refineNumber("1", gg.TYPE_FLOAT)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("3000", gg.TYPE_FLOAT)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+  end
+  
+  function cielonegro()
+    gg.setRanges(gg.REGION_CODE_APP)
+    gg.searchNumber("0.99;0.57735025883;1e-6::13", gg.TYPE_FLOAT)
+    gg.refineNumber("1e-6", gg.TYPE_FLOAT)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("-1", gg.TYPE_FLOAT)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+  end
+  
+  function L7()
+    gg.setRanges(32)
+    gg.searchNumber("h23AAA6B8460ACD70", 1)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("h23AAA6B8B2F71FA4", 1)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.searchNumber("h477B5ABDAE5766BB5C1F48BA1BC0CF3B9CFB283DA2B117BDE4997F3F0400803F0000803FFEFF7F3F", 1)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("h8D07743FAE5766BB5C1F48BA1BC0CF3B9CFB283DA2B117BDE4997F3F000060410000604100006041", 1)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.searchNumber("h4C7B5ABD0A5766BB1E2148BA2AC2CF3B96FB283DE8B117BDE3997F3F0400803F0100803FFCFF7F3F", 1)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("h1B0E743FAE5766BB5C1F48BA1BC0CF3B9CFB283DA2B117BDE4997F3F000060410000604100006041", 1)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.searchNumber("h1000000062006F006E0065005F004C006500660074005F0057006500610070006F006E00", 1)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("h1000000062006F006E0065005F005300700069006E006500000000000000000000000000", 1)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+  end
+  
+  function recoil()
+    gg.setRanges(gg.REGION_CODE_APP)
+    gg.searchNumber("-7.1608877e24;1000;-1.30930378e25::9", gg.TYPE_FLOAT)
+    gg.refineNumber("1000", gg.TYPE_FLOAT)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("0", gg.TYPE_FLOAT)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+  end
+  
+  function aimawm()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("8.40779079e-45F;0D;0F;30F;50F;0.5F::33", gg.TYPE_DWORD)
+    gg.refineNumber("0", gg.TYPE_DWORD)
+    gg.getResults(1)
+    gg.editAll("-1", gg.TYPE_DWORD)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Aim awm")
+  end
+  
+  function switch()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber(",h 00 00 00 00 3f 00 00 80 3e", gg.TYPE_BYTE)
+    gg.getResults(100)
+    gg.editAll(",h 00 ec 51 b8 3d 8f c2 f5 3c", gg.TYPE_BYTE)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("Fast change Sniper")
+  end
+  
+  function aimbotmira()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("1075000115;1065353216::9", gg.TYPE_DWORD)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("-10;981668463", gg.TYPE_DWORD)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+  end
+  
+  function lloc()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 61 00 73 00 73 00 69 00 73 00 74 00 61 00 6E 00 74 00 69 00 74 00 65 00 6D 00 2F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 73 00 68 00 6F 00 70 00", 1)
+    gg.getResults(10000)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00 5F 00 67 00 72 00 65 00 65 00 6E 00", 1)
+    gg.toast("WALL LOJINHA〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h69006E00670061006D0065002F007000690063006B00750070002F007000690063006B00750070005F00740072006500610074006D0065006E007400670075006E00", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+    gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F006C0061007300650072005F0062006C00750065006300610072006400", gg.TYPE_BYTE)
+    gg.toast("LOCAL PISTOLA DE CURA〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h69006E00670061006D0065002F007000690063006B00750070002F006100740074006100630068006D0065006E0074002F007000690063006B00750070005F00730069006C0065006E006300650072005F003100", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+    gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+    gg.editAll("h65006600660065006300740073002F007600660078005F0069006E00670061006D0065005F006C0061007300650072005F0063006100720065007000610063006B00610067006500", gg.TYPE_BYTE)
+    gg.toast("LOCAL SILENCIADOR〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 69 00 74 00 65 00 6D 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 69 00 6E 00 68 00 61 00 6C 00 65 00 72 00", gg.TYPE_BYTE)
+    gg.getResults(400)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 66 00 66 00 5F 00 66 00 78 00 5F 00 67 00 75 00 69 00 64 00 65 00 5F 00 61 00 72 00 72 00 6F 00 77 00", gg.TYPE_BYTE)
+    gg.toast("LOCAL INALADOR〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 63 00 61 00 6E 00 64 00 79 00 2F 00 6F 00 62 00 33 00 31 00 2F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 74 00 6F 00 6B 00 65 00 6E 00 5F 00 6C 00 65 00 73 00 73 00", gg.TYPE_BYTE)
+    gg.getResults(400)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00", gg.TYPE_BYTE)
+    gg.toast("LOCAL MOEDA〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 69 00 74 00 65 00 6D 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 66 00 61 00 6B 00 5F 00 31 00", 1)
+    gg.getResults(10000)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00 5F 00 67 00 72 00 65 00 65 00 6E 00", 1)
+    gg.toast("LOCAL MEDKIT〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 62 00 61 00 67 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 62 00 61 00 67 00 5F 00 33 00", gg.TYPE_BYTE)
+    gg.getResults(400)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 61 00 67 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00 5F 00 73 00 68 00 6F 00 70 00", gg.TYPE_BYTE)
+    gg.toast("LOCAL MOCHILA V3〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 61 00 74 00 74 00 61 00 63 00 68 00 6D 00 65 00 6E 00 74 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 67 00 6F 00 6C 00 64 00 5F 00 6D 00 34 00 61 00 31 00 5F 00 61 00 74 00 74 00 61 00 63 00 68 00 6D 00 65 00 6E 00 74 00", 1)
+    gg.getResults(10000)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00 5F 00 63 00 61 00 72 00 65 00 70 00 61 00 63 00 6B 00 61 00 67 00 65 00", 1)
+    gg.toast("LOCAL CHIP EVOLUÇÃO〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 68 00 65 00 6C 00 6D 00 65 00 74 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 68 00 65 00 6C 00 6D 00 65 00 74 00 5F 00 33 00", gg.TYPE_BYTE)
+    gg.getResults(400)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00 5F 00 63 00 61 00 72 00 65 00 70 00 61 00 63 00 6B 00 61 00 67 00 65 00", gg.TYPE_BYTE)
+    gg.toast("LOCAL CAPACETE LV3〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h 69 00 6E 00 67 00 61 00 6D 00 65 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 2F 00 61 00 72 00 6D 00 6F 00 72 00 2F 00 70 00 69 00 63 00 6B 00 75 00 70 00 5F 00 61 00 72 00 6D 00 6F 00 72 00 5F 00 33 00", gg.TYPE_BYTE)
+    gg.getResults(400)
+    gg.editAll("h 65 00 66 00 66 00 65 00 63 00 74 00 73 00 2F 00 76 00 66 00 78 00 5F 00 69 00 6E 00 67 00 61 00 6D 00 65 00 5F 00 6C 00 61 00 73 00 65 00 72 00", gg.TYPE_BYTE)
+    gg.toast("LOCAL COLETE LV3〘ON〙")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("CUSTOM DATA ❪✔️️❫")
+  end
+  
+  function jsped()
+    gg.getTargetInfo()
+    gg.getRangesList("/data/app/~~tGpDHVPP3nLg-yqtPTyFeA==/com.gspace.android-Gm9GyFWD9g4rOeHn4KLgXg==/lib/arm64/libil2cpp.so")
+    gg.getTargetInfo()
+    gg.getRangesList("/data/app/~~tGpDHVPP3nLg-yqtPTyFeA==/com.gspace.android-Gm9GyFWD9g4rOeHn4KLgXg==/lib/arm64/libil2cpp.so")
+    gg.toast("🕹️SUPER VELOCIDAD")
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.processResume()
+    gg.setVisible(false)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+  end
+  
+  function full()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber(";bone_Spine", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+    gg.getResults(16384)
+    gg.editAll(";bone_Head1", gg.TYPE_WORD)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("-0.0446202829", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(300)
+    gg.getResults(300)
+    gg.editAll("-0.03999999911", gg.TYPE_FLOAT, 0, nil, nil, nil, nil, nil)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("-0.0388151556", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(300)
+    gg.getResults(300)
+    gg.editAll("0.05000000075", gg.TYPE_FLOAT, 0, nil, nil, nil, nil, nil)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.searchNumber("0.07869631797;0.99689865112;1.0;1.0;1.0::17", gg.TYPE_FLOAT)
+    gg.refineNumber("1", gg.TYPE_FLOAT, nil, nil, nil, nil, nil)
+    gg.getResults(999)
+    gg.getResults(999)
+    gg.editAll("-1.5", gg.TYPE_FLOAT, 0, nil, nil, nil, nil, nil)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.searchNumber("0.98958933353;1.0;1.0;1.0::17", gg.TYPE_FLOAT)
+    gg.refineNumber("1", gg.TYPE_FLOAT, nil, nil, nil, nil, nil)
+    gg.getResults(99)
+    gg.getResults(99)
+    gg.editAll("-1.5", gg.TYPE_FLOAT, 0, nil, nil, nil, nil, nil)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.alert("ᕙ(͡°‿ ͡°)ᕗ")
+    gg.setVisible(false)
+     gg.clearResults()-----[[© Dec By メ SHIV MODS  亗 ]] -----
+    gg.toast("╰┈➤🌍HEADSHOT VIP?? |〘ON✅〙")
+  end
+  
+  while true do
+    if gg.isVisible() then
+      gg.setVisible(false)
+      script()
+    end
+end
